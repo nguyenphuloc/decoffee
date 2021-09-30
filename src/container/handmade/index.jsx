3c174f2630f6fc3b82f66eb64ba64914ef4580ec
nguyenphuloc <nguyenphuloc1206@gmail.com>
2021-09-30 18:59:09 +0700

    done handmade in home v.09.30.001

--- src/container/handmade/index.jsx
+++ src/container/handmade/index.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import './styles.scss';
 
-import hero from '../../assets/image/h1_hero1.jpg';
+import Button from '../../component/button/index';
 
 export default function Handmade() {
     return(
         <div className="ctn-handmade">
-            <div className="img">
-                <img src={hero} alt=""/>
+            <div className="handmade">
+                <div className="handmade-content">
+                    <h1>Handmade</h1>
+                    <h1> 
+                        <span>"Cookies"</span>
+                        and 
+                    </h1>
+                    <h1>Coffee Delights</h1>
+                </div>
+                <p>
+                    Consectetur adipiscing elit ornare viverra arcu, sed hendrerit tortor 
+                    rutrum eget. Fusce tempus risus ac dolor pulvinar, id sodales nibh 
+                    lobortis.
+                </p>
+                <Button 
+                    modifier="orange"
+                    children="our menu"
+                />
             </div>
         </div>
     )
