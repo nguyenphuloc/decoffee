d9cde395f1d2b19255c409346f4dace112a1f833
nguyenphuloc <nguyenphuloc1206@gmail.com>
2021-10-02 16:18:05 +0700

    done ourmenu in home v.10.02.001

--- src/container/ourmenu/index.jsx
+++ src/container/ourmenu/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './styles.scss';
 
+import menu from '../../assets/image/menu-shape.png';
+
 export default function OurMenu() {
     return(
         <div className="ctn-our-menu">
@@ -20,7 +22,93 @@ export default function OurMenu() {
                     </div>
                     <div className="menu-item">
                         <h4>
-                            Proin risus erat ……………
+                            Proin risus erat..................................................................
+                            <span>5$</span>
+                        </h4>
+                        <p>
+                            Sed ultrices nisl velit
+                        </p>
+                    </div>
+                    <div className="menu-item">
+                        <h4>
+                            Bacon, Ham, Cumberland, Sausage, Chorizo..................................................................................
+                            <span>5$</span>
+                        </h4>
+                        <p>
+                            Sed ultrices nisl velit
+                        </p>
+                    </div>
+                    <div className="menu-item">
+                        <h4>
+                            "Full" Paris Breakfast 17".............................................................................................
+                            <span>5$</span>
+                        </h4>
+                        <p>
+                            Sed ultrices nisl velit
+                        </p>
+                    </div>
+                    <div className="menu-item">
+                        <h4>
+                            Proin risus erat..................................................................
+                            <span>5$</span>
+                        </h4>
+                        <p>
+                            Sed ultrices nisl velit
+                        </p>
+                    </div>
+                    <div className="menu-item">
+                        <h4>
+                            Three Egg Omelet 17.............................................................................................
+                            <span>5$</span>
+                        </h4>
+                        <p>
+                            Sed ultrices nisl velit
+                        </p>
+                    </div>
+                </div> 
+                <div className="list-drinks">
+                <div className="list">
+                        <h4>Drinks</h4>
+                    </div>
+                    <div className="menu-item">
+                        <h4>
+                            Proin risus erat..................................................................
+                            <span>5$</span>
+                        </h4>
+                        <p>
+                            Sed ultrices nisl velit
+                        </p>
+                    </div>
+                    <div className="menu-item">
+                        <h4>
+                            Bacon, Ham, Cumberland, Sausage, Chorizo..................................................................................
+                            <span>5$</span>
+                        </h4>
+                        <p>
+                            Sed ultrices nisl velit
+                        </p>
+                    </div>
+                    <div className="menu-item">
+                        <h4>
+                            "Full" Paris Breakfast 17".............................................................................................
+                            <span>5$</span>
+                        </h4>
+                        <p>
+                            Sed ultrices nisl velit
+                        </p>
+                    </div>
+                    <div className="menu-item">
+                        <h4>
+                            Proin risus erat..................................................................
+                            <span>5$</span>
+                        </h4>
+                        <p>
+                            Sed ultrices nisl velit
+                        </p>
+                    </div>
+                    <div className="menu-item">
+                        <h4>
+                            Three Egg Omelet 17.............................................................................................
                             <span>5$</span>
                         </h4>
                         <p>
@@ -28,8 +116,59 @@ export default function OurMenu() {
                         </p>
                     </div>
                 </div>
-                <div className="list-drinks"></div>
-                <div className="list-food"></div>
+                <div className="list-food">
+                <div className="list">
+                        <h4>Fast Food</h4>
+                    </div>
+                    <div className="menu-item">
+                        <h4>
+                            Proin risus erat..................................................................
+                            <span>5$</span>
+                        </h4>
+                        <p>
+                            Sed ultrices nisl velit
+                        </p>
+                    </div>
+                    <div className="menu-item">
+                        <h4>
+                            Bacon, Ham, Cumberland, Sausage, Chorizo..................................................................................
+                            <span>5$</span>
+                        </h4>
+                        <p>
+                            Sed ultrices nisl velit
+                        </p>
+                    </div>
+                    <div className="menu-item">
+                        <h4>
+                            "Full" Paris Breakfast 17".............................................................................................
+                            <span>5$</span>
+                        </h4>
+                        <p>
+                            Sed ultrices nisl velit
+                        </p>
+                    </div>
+                    <div className="menu-item">
+                        <h4>
+                            Proin risus erat..................................................................
+                            <span>5$</span>
+                        </h4>
+                        <p>
+                            Sed ultrices nisl velit
+                        </p>
+                    </div>
+                    <div className="menu-item">
+                        <h4>
+                            Three Egg Omelet 17.............................................................................................
+                            <span>5$</span>
+                        </h4>
+                        <p>
+                            Sed ultrices nisl velit
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="menu-img">
+                <img src={menu} alt=""/>
             </div>
         </div>
     )
