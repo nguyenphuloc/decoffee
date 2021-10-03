6b138ec9b13e5888fb5a6bad5fdd9aa8754ce49f
nguyenphuloc <nguyenphuloc1206@gmail.com>
2021-10-03 16:01:29 +0700

    continue footer in home v.10.03.001

--- src/container/footer/index.jsx
+++ src/container/footer/index.jsx
@@ -3,8 +3,13 @@ import './styles.scss';
 
 export default function Footer() {
     return(
-        <div className="footer">
-
+        <div className="ctn-footer">
+            <div className="footer">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className="footer-kirapol"></div>
         </div>
     )
 }
