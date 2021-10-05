d2d85298cafa9fcbb733869044a73bef4b09e7f1
nguyenphuloc <nguyenphuloc1206@gmail.com>
2021-10-05 14:52:00 +0700

    footer in home v.10.05.001

--- src/container/footer/index.jsx
+++ src/container/footer/index.jsx
@@ -5,9 +5,15 @@ export default function Footer() {
     return(
         <div className="ctn-footer">
             <div className="footer">
-                <div className="footer-address"></div>
-                <div className ="footer-logo"></div>
-                <div className="footer-menu"></div>
+                <div className="footer-address">
+                    <div></div>
+                </div>
+                <div className ="footer-logo">
+                    <div></div>
+                </div>
+                <div className="footer-menu">
+                    <div></div>
+                </div>
             </div>
             <div className="footer-author"></div>
         </div>
