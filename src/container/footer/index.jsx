971469ebb8a90e8d1bf0be7b714dbe5597cfd28c
nguyenphuloc <nguyenphuloc1206@gmail.com>
2021-10-08 15:51:55 +0700

    footer in home v.10.08.001

--- src/container/footer/index.jsx
+++ src/container/footer/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './styles.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faVolleyballBall, faCannabis, faPodcast } from '@fortawesome/free-solid-svg-icons'
 
 export default function Footer() {
     return(
@@ -9,7 +11,11 @@ export default function Footer() {
                     <h4>Contact Us</h4>
                     <p>0708 393 969</p>
                     <p>30 Street. Bo Bao Tan Thang, Son Ky, Tan Phu, Ho Chi Minh City</p>
-                    <div></div> 
+                    <div className="footer-icon">
+                        <FontAwesomeIcon icon={faVolleyballBall} />
+                        <FontAwesomeIcon icon={faCannabis} className="icon"/>
+                        <FontAwesomeIcon icon={faPodcast} />    
+                    </div> 
                 </div>
                 <div className ="footer-logo">
                     <div></div>
